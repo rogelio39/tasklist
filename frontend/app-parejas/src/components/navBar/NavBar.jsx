68f3becf1b8dba4de8cee8e5ca0b7643b1ffaf12
Rogelio Suleta <andresrogesu@gmail.com>
2024-10-11 21:03:32 -0300

    se agrego boton para ir a registro

--- frontend/app-parejas/src/components/navBar/NavBar.jsx
+++ frontend/app-parejas/src/components/navBar/NavBar.jsx
@@ -17,11 +17,15 @@ const NavBar = () => {
     const goToLogin = () => {
         navigate('/login')
     }
+    const goToRegister = () => {
+        navigate('/register')
+    }
 
 
     return (
         <div className="nav-container">
             <button onClick={goIndex}>Inicio</button>
+            <button onClick={goToRegister}>registro</button>
             <button onClick={goToLogin}>Login</button>
             <button onClick={goDashboard}>Tabla</button>
 
